2cb43998e8d2e09457ece43b381db0771c2ffa0f
Kristin <1358365@DESKTOP-4FH9G41>
2019-06-06 11:58:30 +0500

    connection to db established

---
+++ mainWindow.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+from App import Ui_MainWindow
+
+class mainwindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mainwindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
